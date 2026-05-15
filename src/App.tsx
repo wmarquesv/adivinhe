e197f0ccb6ed77546300680694f073a80ab42058
wmarquesv <walace.mv@hotmail.com>
2026-05-15 01:07:12 -0300

    adicionado logica para sortear as palavras e deixando dinâmico as tentativas do usuário

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import styles from "./app.module.css";
+import { useEffect, useState } from "react";
+
+import { WORDS, type Challenge } from "./utils/words";
 
 import { Header } from "./components/Header";
 import { Tip } from "./components/Tip";
@@ -8,23 +11,43 @@ import { Button } from "./components/Button";
 import { LettersUsed } from "./components/LettersUsed";
 
 export default function app() {
+  const [attempts, setAttempts] = useState(0);
+  const [letter, setLetter] = useState("");
+  const [challenge, setChallenge] = useState<Challenge | null>(null);
+
   function handleRestartGame() {
     alert("Reiniciar o jogo!");
+  }
+
+  function startGame() {
+    const index = Math.floor(Math.random() * WORDS.length);
+    const randomWord = WORDS[index];
+
+    setChallenge(randomWord);
+
+    setAttempts(0);
+    setLetter("");
+  }
+
+  useEffect(() => {
+    startGame();
+  }, []);
+
+  if (!challenge) {
+    return;
   }
 
   return (
     <div className={styles.container}>
       <main>
-        <Header current={5} max={10} onRestart={handleRestartGame} />
+        <Header current={attempts} max={10} onRestart={handleRestartGame} />
 
         <Tip tip="Uma das linguagens de programação mais utilizadas" />
 
         <div className={styles.word}>
-          <Letter value="R" />
-          <Letter value="e" />
-          <Letter value="a" />
-          <Letter value="c" />
-          <Letter value="t" />
+          {challenge.word.split("").map(() => (
+            <Letter value="" />
+          ))}
         </div>
 
         <h4>Palpite</h4>
